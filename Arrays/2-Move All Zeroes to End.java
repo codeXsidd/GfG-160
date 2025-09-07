7edebcfe2dbb3a51b33c2cd57f854dc4951bb2e0
Siddharth S <179855721+codeXsidd@users.noreply.github.com>
2025-09-07 23:16:22 +0530

    Add files via upload

---
+++ Arrays/2-Move All Zeroes to End.java
@@ -0,0 +1,15 @@
+
+class Solution {
+    void pushZerosToEnd(int[] arr) {
+        // code here
+       int count=0;
+       for(int i=0;i<arr.length;i++){
+           if(arr[i]!=0){
+               int temp=arr[count];
+               arr[count]=arr[i];
+               arr[i]=temp;
+               count++;
+           }
+       }
+    }
+}
